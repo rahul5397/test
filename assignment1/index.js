17989d10bd3de182f2c59ddb44f6b1e17242b918
rahul53 <rahulgupta51997@gmail.com>
2022-09-10 21:27:19 +0530

    jsAssignment
    task

---
+++ assignment1/index.js
@@ -0,0 +1,621 @@
+const foodItem = [
+    {
+      id: 1,
+      name: "Ambur Biryani",
+      category: "biryani",
+      rating: 4.3,
+      price: 13,
+      img: "images/biryani/Ambur-Chicken-Biryani.jpg",
+      quantity: 1,
+    },
+    {
+      id: 2,
+      name: "Hyderabadi Biryani",
+      category: "biryani",
+      rating: 4.3,
+      price: 15,
+      img: "images/biryani/Chicken-Biryani-hyd.jpg",
+      quantity: 1,
+    },
+    {
+      id: 3,
+      name: "Egg Biryani",
+      category: "biryani",
+      rating: 4.3,
+      price: 18,
+      img: "images/biryani/egg-biryani.jpeg",
+      quantity: 1,
+    },
+    {
+      id: 4,
+      name: "Goan Fish Biryani",
+      category: "biryani",
+      rating: 4.3,
+      price: 15,
+      img: "images/biryani/goan-fish-biryani.jpg",
+      quantity: 1,
+    },
+    {
+      id: 5,
+      name: "Mutton Biryani",
+      category: "biryani",
+      rating: 4.3,
+      price: 10,
+      img: "images/biryani/hyd-Mutton-Biryani.jpg",
+      quantity: 1,
+    },
+    {
+      id: 6,
+      name: "Kamrupi Biryani",
+      category: "biryani",
+      rating: 4.3,
+      price: 12,
+      img: "images/biryani/kamrupi-biryani.jpg",
+      quantity: 1,
+    },
+    {
+      id: 7,
+      name: "Kashmiri Biryani",
+      category: "biryani",
+      rating: 4.3,
+      price: 13,
+      img: "images/biryani/kashmiri.pulao.jpg",
+      quantity: 1,
+    },
+    {
+      id: 8,
+      name: "Memoni Biryani",
+      category: "biryani",
+      rating: 4.3,
+      price: 20,
+      img: "images/biryani/memonibiryani.png",
+      quantity: 1,
+    },
+    {
+      id: 9,
+      name: "Mughlai Biryani",
+      category: "biryani",
+      rating: 4.3,
+      price: 15,
+      img: "images/biryani/mughlai-biryani.jpg",
+      quantity: 1,
+    },
+    {
+      id: 10,
+      name: "Chicken Roast",
+      category: "chicken",
+      rating: 4.3,
+      price: 11,
+      img: "images/chicken/Chicken_roast.jpg",
+      quantity: 1,
+    },
+    {
+      id: 11,
+      name: "Chicken Curry",
+      category: "chicken",
+      rating: 4.3,
+      price: 10,
+      img: "images/chicken/Chicken-Curry.jpg",
+      quantity: 1,
+    },
+    {
+      id: 12,
+      name: "Chicken Do Pyaza",
+      category: "chicken",
+      rating: 4.3,
+      price: 14,
+      img: "images/chicken/Chicken-do-Pyaza.jpg",
+      quantity: 1,
+    },
+    {
+      id: 13,
+      name: "Chicken Masala",
+      category: "chicken",
+      rating: 4.3,
+      price: 12,
+      img: "images/chicken/Chicken-Masala.jpeg",
+      quantity: 1,
+    },
+    {
+      id: 14,
+      name: "Handi Chicken",
+      category: "chicken",
+      rating: 4.3,
+      price: 17,
+      img: "images/chicken/Handi-chicken.jpg",
+      quantity: 1,
+    },
+    {
+      id: 15,
+      name: "Murgh Musallam",
+      category: "chicken",
+      rating: 4.3,
+      price: 20,
+      img: "images/chicken/Murgh-Musallam.jpg",
+      quantity: 1,
+    },
+    {
+      id: 16,
+      name: "Matar Paneer",
+      category: "paneer",
+      rating: 4.3,
+      price: 15,
+      img: "images/paneer/Matar-Paneer.jpg",
+      quantity: 1,
+    },
+    {
+      id: 17,
+      name: "Palak Paneer",
+      category: "paneer",
+      rating: 4.3,
+      price: 10,
+      img: "images/paneer/palak-paneer.jpg",
+      quantity: 1,
+    },
+    {
+      id: 18,
+      name: "Paneer Butter Masala",
+      category: "paneer",
+      rating: 4.3,
+      price: 15,
+      img: "images/paneer/paneer-butter-masala.jpg",
+      quantity: 1,
+    },
+    {
+      id: 19,
+      name: "Paneer Do Pyaza",
+      category: "paneer",
+      rating: 4.3,
+      price: 12,
+      img: "images/paneer/Paneer-Do-Pyaza.jpg",
+      quantity: 1,
+    },
+    {
+      id: 20,
+      name: "Hyderabadi Paneer",
+      category: "paneer",
+      rating: 4.3,
+      price: 8,
+      img: "images/paneer/Paneer-Hyderabadi.jpg",
+      quantity: 1,
+    },
+    {
+      id: 21,
+      name: "Paneer Lababdar",
+      category: "paneer",
+      rating: 4.3,
+      price: 7,
+      img: "images/paneer/paneer-lababdar.jpg",
+      quantity: 1,
+    },
+    {
+      id: 22,
+      name: "Shahi Paneer",
+      age: "32",
+      category: "paneer",
+      rating: 4.3,
+      price: 5,
+      img: "images/paneer/shahi-paneer.jpg",
+      quantity: 1,
+    },
+    {
+      id: 23,
+      name: "Navratan Korma",
+      category: "vegetable",
+      rating: 4.3,
+      price: 8,
+      img: "images/vegetable/navratan-korma_-vegetable.png",
+      quantity: 1,
+    },
+    {
+      id: 24,
+      name: "Veg Jalfrezi",
+      category: "vegetable",
+      rating: 4.3,
+      price: 7,
+      img: "images/vegetable/VEG-JALFREZI.jpg",
+      quantity: 1,
+    },
+    {
+      id: 25,
+      name: "Veg Biryani",
+      category: "vegetable",
+      rating: 4.3,
+      price: 5,
+      img: "images/vegetable/vegetable-biryani.jpg",
+      quantity: 1,
+    },
+    {
+      id: 26,
+      name: "Veg Curry",
+      category: "vegetable",
+      rating: 4.3,
+      price: 7,
+      img: "images/vegetable/vegetable-curry.jpeg",
+      quantity: 1,
+    },
+    {
+      id: 27,
+      name: "Veg Kolhapur",
+      category: "vegetable",
+      rating: 4.3,
+      price: 10,
+      img: "images/vegetable/vegetable-kolhapuri.jpg",
+      quantity: 1,
+    },
+    {
+      id: 28,
+      name: "Veg Masala",
+      category: "vegetable",
+      rating: 4.3,
+      price: 4,
+      img: "images/vegetable/vegetable-masala.jpg",
+      quantity: 1,
+    },
+    {
+      id: 29,
+      name: "Veg Pakora",
+      category: "vegetable",
+      rating: 4.3,
+      price: 4,
+      img: "images/vegetable/vegetable-pakora.jpg",
+      quantity: 1,
+    },
+    {
+      id: 30,
+      name: "Momos",
+      category: "chinese",
+      rating: 4.3,
+      price: 8,
+      img: "images/chinese/cabbage-momos-.jpg",
+      quantity: 1,
+    },
+    {
+      id: 31,
+      name: "Chicken Manchurian",
+      category: "chinese",
+      rating: 4.3,
+      price: 7,
+      img: "images/chinese/ChickenManchurian.jpg",
+      quantity: 1,
+    },
+    {
+      id: 32,
+      name: "Chili Chicken",
+      category: "chinese",
+      rating: 4.3,
+      price: 5,
+      img: "images/chinese/Chili-Chicken.jpg",
+      quantity: 1,
+    },
+    {
+      id: 33,
+      name: "Chowmein",
+      category: "chinese",
+      rating: 4.3,
+      price: 16,
+      img: "images/chinese/chowmin.jpg",
+      quantity: 1,
+    },
+    {
+      id: 34,
+      name: "Spring Roll",
+      category: "chinese",
+      rating: 4.3,
+      price: 14,
+      img: "images/chinese/spring-rolls.jpg",
+      quantity: 1,
+    },
+    {
+      id: 35,
+      name: "Szechuan Chicken",
+      category: "chinese",
+      rating: 4.3,
+      price: 10,
+      img: "images/chinese/szechuan-chicken.jpg",
+      quantity: 1,
+    },
+    {
+      id: 36,
+      name: "Fried Rice",
+      category: "chinese",
+      rating: 4.3,
+      price: 8,
+      img: "images/chinese/veg-fried-rice.jpg",
+      quantity: 1,
+    },
+    {
+      id: 37,
+      name: "Butter Masala Dosa",
+      category: "south indian",
+      rating: 4.3,
+      price: 18,
+      img: "images/south indian/Butter-Masala-Dosa.png",
+      quantity: 1,
+    },
+    {
+      id: 38,
+      name: "Idli",
+      category: "south indian",
+      rating: 4.3,
+      price: 20,
+      img: "images/south indian/idli-with-rice-flour.jpg",
+      quantity: 1,
+    },
+    {
+      id: 39,
+      name: "Masala Dosa",
+      category: "south indian",
+      rating: 4.3,
+      price: 12,
+      img: "images/south indian/masala-dosa.jpg",
+      quantity: 1,
+    },
+    {
+      id: 40,
+      name: "Mysore Bonda",
+      category: "south indian",
+      rating: 4.3,
+      price: 10,
+      img: "images/south indian/mysore-bonda.jpg",
+      quantity: 1,
+    },
+    {
+      id: 41,
+      name: "Onion Uttapam",
+      category: "south indian",
+      rating: 4.3,
+      price: 15,
+      img: "images/south indian/onion-uttapam.jpg",
+      quantity: 1,
+    },
+    {
+      id: 42,
+      name: "Plain Dosa",
+      category: "south indian",
+      rating: 4.3,
+      price: 40,
+      img: "images/south indian/plain-dosa.jpeg",
+      quantity: 1,
+    },
+    {
+      id: 43,
+      name: "Rava Uttapam",
+      category: "south indian",
+      rating: 4.3,
+      price: 25,
+      img: "images/south indian/Rava-Uttapam.jpg",
+      quantity: 1,
+    },
+    {
+      id: 44,
+      name: "Sambhar Vada",
+      category: "south indian",
+      rating: 4.3,
+      price: 34,
+      img: "images/south indian/sambhar-vada.jpg",
+      quantity: 1,
+    },
+  ];
+  /**
+   * This function is passed the list of items of any categpry based on users choice.
+   * It then iterates over the list and creates the HTML i.e rows for every item.
+   * It then returns the generated html to displayItems() function
+   */
+
+   var cartDisplay = document.getElementById("cart-page")
+   cartDisplay.style.display="none";
+   var foodDisplay = document.getElementById("food")
+   foodDisplay.style.display = "block";
+
+  function displayItem(item)
+  { 
+    var html =''
+    
+    html+=`
+    <div class="row text-center m-b-5">
+        <div class="col-md-3 text-center itemAlign">
+            <img src="${item.img}" class='img-fluid'/>
+        </div>
+        <div class='col-md-2 itemAlign'>
+            <h4>${item.name}</h4>
+        </div>
+        <div class='col-md-2 itemAlign'>
+            <h4>${item.price}</h4>
+        </div>
+        <div class='col-md-2 itemAlign'>
+            <h4>${item.rating}</h4>
+        </div>
+        <div class='col-md-2 itemAlign'>
+            <input type="checkbox" id="addToCartChecked${item.id}" value="${item.id}" onclick="addToCart(this.value)">
+        </div> 
+        <hr style='color:red; height:2px'>  
+    </div>
+    `
+    
+    return html;
+  }
+  /**
+   * this function is called when a particular item is selected from 
+   * dropdown. based on the category it fetches all the items and invokes
+   * displayItem function passing the list of items for that category.
+   * The returned HTML from displayItem() is then displayed on the main page
+   */
+
+
+
+  function displayItems() {
+    let dropdownValue = document.getElementById("category").value;
+   
+    var filteredValue = foodItem.filter(item => item.category == dropdownValue);
+    //console.log(filteredValue)
+    var row ='';
+    for(i=0; i<filteredValue.length; i++)  
+        {
+          row += displayItem(filteredValue[i]);
+         
+        }
+        document.getElementById("items").innerHTML = row;
+
+    
+  }
+  // call this function to display the 1st item in the dropdown
+  displayItems();
+
+
+  // create an empty array to store the item user selects to be added in the cart.
+  let count = 0;
+  let idItemArray = [];
+
+   /**
+   This function is called when the user ticks the checkbox and it does the following:
+   1. Gets the id of the item checked as parameter and looks up for the item in the foodItem array
+   2. Then checks if item is not present in the cart then add the item in the cart.
+   3. If the item is already present in the cart then display an alert saying, item already present in the cart.
+  
+   */
+  var cartData=[]
+  function addToCart(id)
+  {
+    
+    var itemToAdd = foodItem.find((item) => item.id == id);
+
+    function isInArray(value, array)
+    {
+      return array.indexOf(value) > -1;
+    }
+    var found = isInArray(id, idItemArray);
+    
+    
+    if(found > 0)
+    {
+      alert("This item is already exist in Cart");
+      document.getElementById("addToCartChecked"+id).checked= true; 
+    }
+    else
+    {
+      idItemArray.push(id)
+      count++;
+      document.getElementById("cart-plus").innerHTML = ` ${count} Items`;
+
+      cartData.push(itemToAdd)
+      console.log(cartData);
+    }
+    
+  }
+
+
+//let totalPrice=0;
+//var quantity=1;
+  function cartPage()
+  {
+
+    
+
+
+    cartDisplay.style.display="block";
+    foodDisplay.style.display = "none";
+    console.log(idItemArray)
+    var row = "";
+    for(var i=0; i<idItemArray.length; i++)
+    {
+      var cartArray = foodItem.filter(item => item.id == idItemArray[i]);
+      console.log(cartArray)
+      row += displayCartItem(cartArray[0]);
+   
+    }
+    document.getElementById("table-body").innerHTML = row;
+
+    if(cartData.length == 0)
+    {
+    //console.log('hello')
+    cartDisplay.style.display = "none"
+    alert("there is nothing to show in your card. Add something in your cart")
+    foodDisplay.style.display = "block";
+    }
+
+  }
+
+  function displayCartItem(item)
+    {
+      //console.log(item.name)
+      
+      html =''
+      html += `
+      <tr id="rowId${item.id}"  style="border-bottom: 1px solid red">
+        <td> <img src="${item.img}"></td>
+        <td>${item.name}</td>
+        <td id="xyz${item.id}">
+          <button onclick="increment(${item.id},${item.price})">+</button>${item.quantity}<button onclick="decrement(${item.id},${item.price})">-</button>
+        </td>
+        <td id="abc${item.id}">${item.price}</td>
+       
+      </tr> 
+      `
+      return html;
+    }
+
+
+
+
+
+  function increment(id,price)
+  { 
+    var itemInCart = cartData.find((item)=>item.id == id)
+    itemInCart.quantity +=1;
+    itemInCart.price = price * itemInCart.quantity
+    console.log(itemInCart.price)
+    document.getElementById("xyz"+id).innerHTML = `<button  onclick="increment(${id},${price})">+</button>${itemInCart.quantity}<button onclick="decrement(${id},${price})">-</button>`
+    document.getElementById("abc"+id).innerHTML= itemInCart.quantity * price;
+    totalAmount();
+     
+  }
+
+  function decrement(id,price)
+  {
+    var itemInCart = cartData.find((item)=>item.id == id)
+    itemInCart.quantity -=1;
+    itemInCart.price = price * itemInCart.quantity
+    var x = cartData.indexOf(itemInCart)
+    if(itemInCart.quantity > 0)
+    {
+      document.getElementById("xyz"+id).innerHTML = `<button  onclick="increment(${id},${price})">+</button>${itemInCart.quantity}<button onclick="decrement(${id},${price})">-</button>`
+      document.getElementById("abc"+id).innerHTML= itemInCart.quantity * price;
+      totalAmount()
+    }
+    else{
+      cartData.splice(x,1);
+      console.log(cartData);
+      document.getElementById("cart-plus").innerHTML = `${cartData.length} Items`
+      document.getElementById("rowId"+id).style.display = "none" 
+      totalAmount()
+    }
+
+    if(cartData.length == 0)
+    {
+      console.log('hello')
+      //document.getElementById("cart-page").style.display = "none"
+      document.getElementById("table-body").innerHTML = `<h1> There is no item to display in your cart. Add something to purchase <\h1>`
+    }
+    
+  }
+
+  
+
+  function totalAmount()
+  {
+    var totalAmountOfAllItems = 0;
+    cartData.map((item) =>
+    {
+      totalAmountOfAllItems+= item.price
+    } )
+    console.log(totalAmountOfAllItems)
+    document.getElementById("total-price").innerHTML = `Total Amount ${totalAmountOfAllItems}`
+  }
+  
+
+  
+  
+  
+ 
+  
